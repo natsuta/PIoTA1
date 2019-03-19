c7e34749aa4fb5d4b1469a4eabb4a21072439b97
Sherman Wong <s3656264@student.rmit.edu.au>
2019-03-20 09:35:27 +1100

    bluetooth thing in progress

--- bluetoothp.py
+++ bluetoothp.py
@@ -1,16 +1,26 @@
-import bluetooth
+from bluetooth import *
+from pprint import pprint
 
-target_name = "Galaxy A8 (2018)"
+target_name = 'Galaxy A8 (2018)'
 target_address = None
 
-nearby_devices = bluetooth.discover_devices(lookup_names = True)
-
-for bdaddr in nearby_devices:
-	if target_name == bluetooth.lookup_name(bdaddr):
-		target_address = bdaddr
-		break
+while target_address is None:
+	nearby_devices = discover_devices(lookup_names = True)
+	print("%d devices found" % len(nearby_devices))
+	for bdaddr, bname in nearby_devices:
+		print(bdaddr, bname)
+		if target_name == bname:
+			target_address = bdaddr
+			break
 
 if target_address is not None:
 	print("found target bluetooth device with address", target_address)
 else:
 	print("could not find target bluetooth device nearby")
+
+port = 3
+s = BluetoothSocket(RFCOMM)
+s.connect((target_address, port))
+
+s.send("Test")
+s.close()
