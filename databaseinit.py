67a4384a63687fd733a7505cd33bdfbe553f559b
Sherman Wong <s3656264@student.rmit.edu.au>
2019-03-27 10:03:41 +1100

    getting that analytics going fam

---
+++ databaseinit.py
@@ -0,0 +1,7 @@
+import sqlite3
+import sys
+con = sqlite3.connect('weather.db')
+with con: 
+    cur = con.cursor() 
+    cur.execute("DROP TABLE IF EXISTS SENSEHAT_data")
+    cur.execute("CREATE TABLE SENSEHAT_data(timestamp DATETIME, temp NUMERIC)")
--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -1,1 +1,12 @@
-print("Test")
+import sense_hat, datetime, sqlite3
+dbname='weather.db'
+
+sense = sense_hat.SenseHat()
+currentDT = datetime.datetime.now()
+temp = sense.get_temperature()
+humid = sense.get_humidity()
+
+print("Date: " + currentDT.strftime("%Y/%m/%d"))
+print("Time: " + currentDT.strftime("%H:%M:%S"))
+print("Temperature: " + (string) temp)
+print("Humidity: " + humidity)
