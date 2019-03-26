77aea4a2d849d2ceeab80f2c2f787d39ea79a2c8
Sherman Wong <s3656264@student.rmit.edu.au>
2019-03-27 10:17:16 +1100

    started weather logging to database

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -1,12 +1,27 @@
-import sense_hat, datetime, sqlite3
+import sense_hat, datetime, time, sqlite3
 dbname='weather.db'
-
 sense = sense_hat.SenseHat()
-currentDT = datetime.datetime.now()
-temp = sense.get_temperature()
-humid = sense.get_humidity()
 
-print("Date: " + currentDT.strftime("%Y/%m/%d"))
-print("Time: " + currentDT.strftime("%H:%M:%S"))
-print("Temperature: " + (string) temp)
-print("Humidity: " + humidity)
+# get data from SenseHat
+def getData():
+	currentDT = datetime.datetime.now()
+	temp = sense.get_temperature()
+	humid = sense.get_humidity()
+	
+	if temp is not None and humid is not None:
+		printData(currentDT, temp, humid)
+		logData(currentDT, temp, humid)
+
+# Prints out the information for confirmation purposes
+def printData(currentDT, temp, humid):
+	print("Date: " + currentDT.strftime("%Y/%m/%d"))
+	print("Time: " + currentDT.strftime("%H:%M:%S"))
+	print("Temperature: " + (string) temp)
+	print("Humidity: " + humid)
+
+# log data into database
+def logData(currentDT, temp, humid):
+	
+def main():
+	getData()
+	
