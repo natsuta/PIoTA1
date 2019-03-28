5f865eba2b10bb0cb0a62998459da136e7b204be
Sherman Wong <s3656264@student.rmit.edu.au>
2019-03-28 22:26:52 +1100

    Finished Bluetooth Pushbullet file for task c

--- bluetoothp.py
+++ bluetoothp.py
@@ -1,30 +1,64 @@
 from bluetooth import *
 from pprint import pprint
 from pushbullet import Pushbullet
-import socket
+import sense_hat
 
+sense = sense_hat.SenseHat()
 pb = Pushbullet("o.KFPP3JCPriu5ydbOQYan1jqCSSP3jia6")
-
 target_name = "Galaxy A8 (2018)"
-target_address = None
 
-while target_address is None:
-	nearby_devices = discover_devices(lookup_names = True)
-	print("%d devices found" % len(nearby_devices))
-	for bdaddr, bname in nearby_devices:
-		print(bdaddr, bname)
-		if target_name == bname:
-			target_address = bdaddr
-			break
+def getData():
+	temp = sense.get_temperature()
+	humid = sense.get_humidity()
+	
+	temp = round(temp, 1)
+	humid = round(humid, 1)
+	
+	search(target_name)
+	send_notif(temp, humid)
+	
+def search(target_name):
+	target_address = None
+	while target_address is None:
+		nearby_devices = discover_devices(lookup_names = True)
+		for bdaddr, bname in nearby_devices:
+			if target_name == bname:
+				target_address = bdaddr
+				break
 
-if target_address is not None:
-	print("found target bluetooth device with address", target_address)
-else:
-	print("could not find target bluetooth device nearby")
+	if target_address is not None:
+		print("found target bluetooth device with address", target_address)
+	else:
+		print("could not find target bluetooth device nearby")
 
-port = 3
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((target_address, port))
+def send_notif(temp, humid):
+	minTemp = 20
+	maxTemp = 30
+	minHumid = 50
+	maxHumid = 60
+	
+	line1 = "Temperature: {}".format(temp)
+	line2 = "Humidity: {}".format(humid)
+	line3a = "Temperature in optimum range"
+	line3b = "Temperature out of optimum range"
+	line4a = "Humidity in optimum range"
+	line4b = "Humidity out of optimum range"
+	
+	msgbody = "{}\n{}".format(line1, line2)
+	
+	if temp < minTemp or temp > maxTemp:
+		msgbody = "{}\n{}".format(msgbody, line3b)
+	else:
+		msgbody = "{}\n{}".format(msgbody, line3a)
+		
+	if humid < minHumid or humid > maxHumid:
+		msgbody = "{}\n{}".format(msgbody, line4b)
+	else:
+		msgbody = "{}\n{}".format(msgbody, line4a)	
+	
+	push = pb.push_note("Weather Report", msgbody)
 
-s.send(bytes("Test", "UTF-8"))
-s.close()
+def main():
+	getData()
+	
+main()
