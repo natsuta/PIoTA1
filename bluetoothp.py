061e4a234d45df8a282f32da9fb2dbed30ac13da
Sherman Wong <s3656264@student.rmit.edu.au>
2019-03-31 17:28:51 +1100

    finally got the json variables to work

--- bluetoothp.py
+++ bluetoothp.py
@@ -1,7 +1,7 @@
 from bluetooth import *
 from pprint import pprint
 from pushbullet import Pushbullet
-import sense_hat
+import sense_hat, json
 
 sense = sense_hat.SenseHat()
 pb = Pushbullet("o.KFPP3JCPriu5ydbOQYan1jqCSSP3jia6")
@@ -19,6 +19,7 @@ def getData():
 	
 def search(target_name):
 	target_address = None
+	print("Searching for Bluetooth device", target_name)
 	while target_address is None:
 		nearby_devices = discover_devices(lookup_names = True)
 		for bdaddr, bname in nearby_devices:
@@ -27,16 +28,11 @@ def search(target_name):
 				break
 
 	if target_address is not None:
-		print("found target bluetooth device with address", target_address)
+		print("Found target Bluetooth device with address", target_address)
 	else:
-		print("could not find target bluetooth device nearby")
+		print("Could not find target Bluetooth device nearby")
 
 def send_notif(temp, humid):
-	minTemp = 20
-	maxTemp = 30
-	minHumid = 50
-	maxHumid = 60
-	
 	line1 = "Temperature: {}".format(temp)
 	line2 = "Humidity: {}".format(humid)
 	line3a = "Temperature in optimum range"
@@ -46,17 +42,20 @@ def send_notif(temp, humid):
 	
 	msgbody = "{}\n{}".format(line1, line2)
 	
-	if temp < minTemp or temp > maxTemp:
-		msgbody = "{}\n{}".format(msgbody, line3b)
-	else:
-		msgbody = "{}\n{}".format(msgbody, line3a)
-		
-	if humid < minHumid or humid > maxHumid:
-		msgbody = "{}\n{}".format(msgbody, line4b)
-	else:
-		msgbody = "{}\n{}".format(msgbody, line4a)	
+	with open('config.json', 'r') as json_file:
+		data = json.load(json_file)
+		if temp < data['min_temperature'] or temp > data['max_temperature']:
+			msgbody = "{}\n{}".format(msgbody, line3b)
+		else:
+			msgbody = "{}\n{}".format(msgbody, line3a)
+			
+		if humid < data["min_humidity"] or humid > data["max_humidity"]:
+			msgbody = "{}\n{}".format(msgbody, line4b)
+		else:
+			msgbody = "{}\n{}".format(msgbody, line4a)	
 	
 	push = pb.push_note("Weather Report", msgbody)
+	print("Notification sent")
 
 def main():
 	getData()
