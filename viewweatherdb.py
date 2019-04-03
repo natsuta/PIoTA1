d04461079c938885e6913360d242d027b8ad0bac
Sherman Wong <s3656264@student.rmit.edu.au>
2019-04-03 22:09:46 +1100

    just some corrections

--- viewweatherdb.py
+++ viewweatherdb.py
@@ -3,11 +3,11 @@ dbname='/home/pi/Documents/PIoTA1/weather.db'
 
 class view:
 	def displayData():
-	conn=sqlite3.connect(dbname)
-	curs=conn.cursor()
-	print ("\nEntire database contents:\n")
-	for row in curs.execute("SELECT * FROM SenseHat_data"):
-		print (row)
-	conn.close()
+		conn=sqlite3.connect(dbname)
+		curs=conn.cursor()
+		print ("\nEntire database contents:\n")
+		for row in curs.execute("SELECT * FROM SenseHat_data"):
+			print (row)
+		conn.close()
 	
 view.displayData()
