cd4a6aefb36c3890c5893feea77073afa308a512
Thomas Rake <s3602236@student.rmit.edu.au>
2019-04-03 12:34:30 +1100

    Updated status and applying message

--- createReport.py
+++ createReport.py
@@ -1,29 +1,34 @@
 ### Generating the report.csv
 ## Modules
-import csv
-import pandas as pd
-import sqlite3
+import csv, pandas as pd, sqlite3, json
    
 # Connecting to a Database and reading data using Pandas, then creating a data frame
 conn = sqlite3.connect("weather.db")
 dataframe = pd.read_sql_query("SELECT * FROM SENSEHAT_data", conn)
 filename = input('Enter a filename (click enter to keep default): ') + '.csv' or 'report.csv'
+
+with open('config.json', 'r') as json_file:
+		data = json.load(json_file) 
+    
+print(data["max_temperature"])
+
 status = pd.Series([])
 for i in range(len(dataframe)):
-    if dataframe["temp"][i] >= 30:
-        status[i]="BAD"
-    elif dataframe["temp"][i] <= 20:
-        status[i]="BAD"
-    elif dataframe["humid"] >= 60:
-        status[i]="BAD"
-    elif dataframe["humid"] <= 50:
-        status="BAD"
+    if dataframe["temp"][i] >= data["max_temperature"]:
+        status[i]="BAD" + ':' + ' ' + 'Temperature is High'
+    elif dataframe["temp"][i] <= data["min_temperature"]:
+        status[i]="BAD" + ':' + ' ' + 'Temperature is Low'
+    elif dataframe["humid"] >= data["max_humidity"]:
+        status[i]="BAD" + ':' + ' ' + 'Humidity is High'
+    elif dataframe["humid"] <= data["min_humidity"]:
+        status="BAD" + ':' + ' ' + 'Temperature is Low'
     else:
         status[i]="OK"
 
 dataframe.insert(3, 'Status', status)
+#dataframe.insert(4, 'Message', message)
 dataframe.to_csv(filename, date_format='%s', index=0)
-print(dataframe)
+#print(dataframe)
 conn.close()
 
 
