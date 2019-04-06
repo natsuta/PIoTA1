9bc262ed7ada65af94d0398ec2ea9476302e0a16
Thomas Rake <s3602236@student.rmit.edu.au>
2019-04-06 17:10:39 +1100

    FIxed bug in createReport

--- createReport.py
+++ createReport.py
@@ -1,9 +1,14 @@
-### Generating the report.csv
-## Modules
-import csv, pandas as pd, sqlite3, json, time
+# Generating the report.csv
+# Modules
+import csv
+import pandas as pd
+import sqlite3
+import json
+import time
 
 # Loading the JSON Variables into memory to perform status operation
-JSONFilename = input("Enter Config File Name (if config.json press enter): ") or 'config.json'
+JSONFilename = input(
+    "Enter Config File Name (if config.json press enter): ") or 'config.json'
 JSONFilename + '.json'
 with open(JSONFilename, 'r') as json_file:
     data = json.load(json_file)
@@ -12,32 +17,28 @@ with open(JSONFilename, 'r') as json_file:
 conn = sqlite3.connect("weather.db")
 dataframe = pd.read_sql_query("SELECT * FROM SENSEHAT_data", conn)
 # Creating an iterable series to have a dynamic Status
-try:
-    status = pd.Series([])
-    for i in range(len(dataframe)):
-        if dataframe["temp"][i] >= data["max_temperature"]:
-            status[i]="BAD" + ':' + ' ' + 'Temperature is High'
-        elif dataframe["temp"][i] <= data["min_temperature"]:
-            status[i]="BAD" + ':' + ' ' + 'Temperature is Low'
-        elif dataframe["humid"] >= data["max_humidity"]:
-            status[i]="BAD" + ':' + ' ' + 'Humidity is High'
-        elif dataframe["humid"] <= data["min_humidity"]:
-            status="BAD" + ':' + ' ' + 'Temperature is Low'
-        else:
-            status[i]="OK"
-except:
-    print("Can not determine weather values")
+status = pd.Series([])
+for i in range(len(dataframe)):
+    if dataframe["temp"][i] >= data["max_temperature"]:
+        status[i] = "BAD" + ':' + ' ' + 'Temperature is High'
+    elif dataframe["temp"][i] <= data["min_temperature"]:
+        status[i]="BAD" + ':' + ' ' + 'Temperature is Low'
+    elif dataframe["humid"][i] >= data["max_humidity"]:
+        status[i]="BAD" + ':' + ' ' + 'Humidity is High'
+    elif dataframe["humid"][i] <= data["min_humidity"]:
+        status="BAD" + ':' + ' ' + 'Temperature is Low'
+    else:
+        status[i]="OK: " + ' ' + 'Readings are Normal'
 # Inserting Status & Message Column
 dataframe.insert(3, 'Status', status)
 # Optional user generated file name
-filename = input('Enter a filename (click enter to keep default): ') or 'report.csv'
-filename + '.csv'
+filename = input('Enter a filename (click enter to keep default): ') + '.csv' or 'report.csv' 
 # File Creation
 dataframe.to_csv(filename, index=0)
 print("Creating Report...")
 time.sleep(0.5)
 print("Report Created, Name: " + filename)
-#print(dataframe)
+# print(dataframe)
 conn.close()
 
 
