1b7e7b331127dd36c8524dc44b2a7d6f4ac0ad69
TheTomRake <tomrakey@gmail.com>
2019-04-07 20:28:01 +1000

    Updated Analytics

--- analytics.py
+++ analytics.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import time
 
 class Main:
     graphData = pd.read_csv('analytics.csv')
@@ -15,23 +15,25 @@ class Main:
 
     @staticmethod
     def main():
+        print('Generating Images...')
         Main.createImageOne()
         Main.createImageTwo()
 
     @staticmethod
     def createImageOne():
         """Using Matplotlib"""
-        plt.scatter(Main.humidity, Main.temp, edgecolors='face', alpha=0.5)
-        plt.xlabel('Humidity')
-        plt.ylabel('Temp')
-        plt.title('Temperature vs Humidity')
-        plt.show()
+        plt_plot = plt.scatter(Main.humidity, Main.temp, edgecolors='face', alpha=0.5)
+        plt.xlabel('Humidity'), plt.ylabel('Temp'), plt.title('Test')
+        fig1 = plt_plot.get_figure()
+        fig1.savefig('Image1.png', bbox_inches='tight')
 
     @staticmethod
     def createImageTwo():
         """Using Seaborn"""
-        sns.swarmplot(x='Temp', y='Status', data=Main.graphData)
-        plt.show()
+        sns_plot = sns.swarmplot(x='Temp', y='Humid', data=Main.graphData)
+        fig2 = sns_plot.get_figure()
+        fig2.savefig('Image2.png', bbox_inches='tight')
+
 
 
 Main.main()
