556f43b70751dca8a61408fce76719d5966d1dc8
TheTomRake <tomrakey@gmail.com>
2019-04-07 18:57:23 +1000

    Completed Analytics

--- analytics.py
+++ analytics.py
@@ -4,17 +4,34 @@ import csv
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def main():
-  graphData = pd.read_csv('analytics.csv')
-  Date = graphData['Date']
-  temp = graphData['Temp']
-  plt.scatter(Date, temp, edgecolors='r')
-  plt.xlabel('Timestamp')
-  plt.ylabel('Temp')
-  plt.title('Temperature data from Pi')
-  plt.show()
 
-if __name__ == "__main__":
-    main()
+class Main:
+    graphData = pd.read_csv('analytics.csv')
+    temp = graphData['Temp']
+    humidity = graphData['Humid']
+    Status = graphData['Status']
 
+    @staticmethod
+    def main():
+        Main.createImageOne()
+        Main.createImageTwo()
+
+    @staticmethod
+    def createImageOne():
+        """Using Matplotlib"""
+        plt.scatter(Main.humidity, Main.temp, edgecolors='face', alpha=0.5)
+        plt.xlabel('Humidity')
+        plt.ylabel('Temp')
+        plt.title('Temperature vs Humidity')
+        plt.show()
+
+    @staticmethod
+    def createImageTwo():
+        """Using Seaborn"""
+        sns.swarmplot(x='Temp', y='Status', data=Main.graphData)
+        plt.show()
+
+
+Main.main()
